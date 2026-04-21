10dd297f885e41ea092195d88a731767b54af4b1
lingwh <lingwh.walkman@homail.com>
2026-04-21 23:57:01 +0800

    修改项目结构

--- 001-c-tutorial/16-动态内存管理/05_结构体+动态内存管理实现链表/src/main.c
+++ 001-c-tutorial/16-动态内存管理/05_结构体+动态内存管理实现链表/src/main.c
@@ -1,7 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct ListNode
+{
+	int data;
+	struct ListNode *next;
+};
+
+struct ListNode *init_list()
+{
+	struct ListNode *head = (struct ListNode *)malloc(sizeof(struct ListNode));
+	if (NULL == head)
+	{
+		exit(-1);
+	}
+	head->next = NULL;
+	return head;
+}
+
+/**
+ * 头插法，最先插入节点在链表尾部
+ * @param head
+ * @param data
+ */
+void insert(struct ListNode *head, int data)
+{
+	assert(head != NULL);
+	struct ListNode *current = (struct ListNode *)malloc(sizeof(struct ListNode));
+	if (NULL == current)
+	{
+		exit(1);
+	}
+	current->data = data;
+	current->next = head->next;
+	head->next = current;
+}
+
+void print_linked_list(struct ListNode *head)
+{
+	assert(head != NULL);
+	struct ListNode *temp = head->next;
+	while (NULL != temp)
+	{
+		printf("%d ", temp->data);
+		temp = temp-> next;
+	}
+}
 
 int main()
 {
-
+	struct  ListNode *head = init_list();
+	for (int i = 1; i <= 10; i++)
+	{
+		insert(head, i);
+	}
+	print_linked_list(head);
 	return 0;
 }
